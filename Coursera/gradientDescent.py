8ec99ec2856abf15fa76be3f9f3962a6313bbf26
Nassir <khishmann@gmail.com>
2022-01-31 21:43:28 -0500

    changed notation to resemble that of Ng's

--- Coursera/gradientDescent.py
+++ Coursera/gradientDescent.py
@@ -18,29 +18,10 @@ DEF_LEARNING_RATE = 0.001
 
 
 def scale_features(x_data):
-    """Scale features to even out descent
-    x_data = [
-        (101, 2),
-        (99, 5),
-        (22, 4)
-    ]
-
-    returns =
-    [
-        ((101 - 74)/79, (2-3.67)/3),
-        ((99 - 74)/69, (5-3.67)/3),
-        ((22 - 74)/69, (4-3.67)/3),
-    ]
-    =
-    [
-        (0.3418, -0.56)
-        (0.316, 0.44),
-        (-0.659, 0.11)
-    ]
-    """
-    n_features = len(x_data[0])
+    """Scale features to even out descent"""
+    n_features = len(x_data)
     for i in range(1, n_features):
-        x_data[:, i] = (x_data[:, i] - np.mean(x_data[:,i])) / (np.max(x_data[:, i]) - np.min(x_data[:, i]))
+        x_data[i] = (x_data[i] - np.mean(x_data[i])) / (np.max(x_data[i]) - np.min(x_data[i]))
     return x_data
 
 
@@ -58,9 +39,9 @@ def descent(X, targets, theta, **kwargs):
             cost > T_COST and
             n_its < MAX_ITS 
         ):
-        predictions = X.dot(theta.T)
+        predictions = theta.T.dot(X)
         errors = predictions - targets
-        delta = (LRN_RT / m) * errors.dot(X)
+        delta = (LRN_RT / m) * X.dot(errors)
         theta = theta - delta
         cost = errors.T.dot(errors)/(2*m)
         cost_history = np.append(cost_history, cost)
@@ -70,7 +51,7 @@ def descent(X, targets, theta, **kwargs):
         *[f"p{i}={round(p,2)}" for i, p in enumerate(theta)],
         f"cost={round(cost, 2)}", 
         n_its,
-        cost_history
+        # cost_history
     ]
 
 
@@ -84,18 +65,12 @@ def linear_noisy_factory(noise_sd):
     return linear_noisy
 
 
-def test():
-    assert \
-        all((np.round_(scale_features(np.array([(1,101,2), (1,99,5), (1,22,4)], dtype="float64")), 2) == \
-            np.array([(1,0.34, -0.56), (1,0.32, 0.44), (1,-0.66, 0.11)])).reshape(-1)
-        ), \
-        "Something's wrong with feature scaling"
-
-
-x_data = np.array(list(zip(
-  [29, 78, 18, 54, 89, 80, 35, 86, 41, 70, 89, 3, 55, 84, 9, 49, 41, 2, 47, 26, 72, 10, 65, 24, 18, 46, 24, 44, 93, 74, 18, 79, 79, 35, 36, 67, 21, 5, 2, 94, 62, 50, 26, 21, 56, 14, 36, 55, 70, 73, 76, 7, 45, 38, 6, 92, 34, 74, 89, 47, 27, 29, 54, 71, 31, 83, 19, 29, 81, 86, 56, 45, 47, 52, 42, 60, 72, 12, 9, 61, 17, 69, 69, 27, 58, 79, 35, 95, 7, 8, 41, 54, 31, 14, 59, 89, 15, 27, 59, 22],
-  [23, 19, 36, 41, 69, 8, 67, 40, 27, 20, 54, 75, 75, 42, 33, 92, 22, 13, 76, 12, 1, 94, 3, 55, 87, 24, 49, 5, 69, 85, 99, 65, 56, 55, 77, 57, 94, 65, 50, 76, 7, 92, 67, 7, 95, 42, 71, 40, 11, 8, 38, 16, 77, 42, 22, 49, 24, 90, 0, 0, 89, 7, 21, 16, 39, 44, 52, 80, 1, 96, 72, 8, 42, 5, 43, 87, 30, 36, 2, 31, 5, 46, 34, 83, 17, 1, 1, 27, 18, 40, 23, 95, 58, 78, 69, 5, 14, 83, 0, 21],
-  )),dtype="float64"
+x_data = np.array(
+    [
+        [29, 78, 18, 54, 89, 80, 35, 86, 41, 70, 89, 3, 55, 84, 9, 49, 41, 2, 47, 26, 72, 10, 65, 24, 18, 46, 24, 44, 93, 74, 18, 79, 79, 35, 36, 67, 21, 5, 2, 94, 62, 50, 26, 21, 56, 14, 36, 55, 70, 73, 76, 7, 45, 38, 6, 92, 34, 74, 89, 47, 27, 29, 54, 71, 31, 83, 19, 29, 81, 86, 56, 45, 47, 52, 42, 60, 72, 12, 9, 61, 17, 69, 69, 27, 58, 79, 35, 95, 7, 8, 41, 54, 31, 14, 59, 89, 15, 27, 59, 22],
+        [23, 19, 36, 41, 69, 8, 67, 40, 27, 20, 54, 75, 75, 42, 33, 92, 22, 13, 76, 12, 1, 94, 3, 55, 87, 24, 49, 5, 69, 85, 99, 65, 56, 55, 77, 57, 94, 65, 50, 76, 7, 92, 67, 7, 95, 42, 71, 40, 11, 8, 38, 16, 77, 42, 22, 49, 24, 90, 0, 0, 89, 7, 21, 16, 39, 44, 52, 80, 1, 96, 72, 8, 42, 5, 43, 87, 30, 36, 2, 31, 5, 46, 34, 83, 17, 1, 1, 27, 18, 40, 23, 95, 58, 78, 69, 5, 14, 83, 0, 21],
+    ]
+    ,dtype="float64"
 )
 
 examples = [
@@ -118,16 +93,20 @@ examples = [
 ]
 
 def run():
-    test()
-
     performance = []
     for init_p, real_p, X, lrn_rt in examples:
+        # best practice: parameters are usually input as a column, 
+        # Note: in numpy, transposing a vector makes no difference
+        init_p = np.array(init_p).T
+        real_p = np.array(real_p).T 
         X = X.copy()
+        m = len(X[0])
+        X = np.vstack((np.ones(m), X))
+        targets = np.array(real_p).T.dot(X)
         # X = scale_features(X)
-        X = np.hstack((np.ones((len(X),1)), X))
-        targets = X.dot(np.array(real_p).T)
+
         start = time.time()
-        res = descent(X, targets, np.array(init_p), lrn_rt=lrn_rt)
+        res = descent(X, targets, init_p, lrn_rt=lrn_rt)
         time_elapsed = time.time() - start
         performance.append((res, round(time_elapsed,2)))
     pprint(performance)
